dd2d3dbf9298a1752c2c1c9776a756873d8eb65c
ShopnilIsCoding <shopnilkhan766@gmail.com>
2024-02-08 14:11:20 +0600

    Four Task Done

--- tasks.js
+++ tasks.js
@@ -6,22 +6,22 @@ function calculateMoney(ticketSale) {
         return (ticketSale * 120) - 900;
 }
 
-console.log(calculateMoney(10)) ;
-console.log(calculateMoney(1055)) ;
-console.log(calculateMoney(93)) ;
-console.log(calculateMoney(-130)) ;
+console.log(calculateMoney(10));
+console.log(calculateMoney(1055));
+console.log(calculateMoney(93));
+console.log(calculateMoney(-130));
 
 //task-2
 
 function checkName(name) {
-    if (typeof(name) !== "string")
+    if (typeof (name) !== "string")
         return "invalid";
-    const newname= name.toLowerCase();
-    const last = newname[newname.length-1];
-    if(last!=='a' && last!=='e' && last!=='i' && last!=='o' && last!=='u' && last!=='w' && last!=='y')
-    return "Bad Name";
+    const newname = name.toLowerCase();
+    const last = newname[newname.length - 1];
+    if (last !== 'a' && last !== 'e' && last !== 'i' && last !== 'o' && last !== 'u' && last !== 'w' && last !== 'y')
+        return "Bad Name";
     else
-    return "Good Name";
+        return "Good Name";
 }
 
 console.log(checkName("salman"));
@@ -32,22 +32,39 @@ console.log(checkName(["Rashed"]));
 
 // task-3
 
-function deleteInvalids(array)
-{
-    if(!Array.isArray(array))
-    return "Invalid Array";
+function deleteInvalids(array) {
+    if (!Array.isArray(array))
+        return "Invalid Array";
     let newArray = [];
-    for(let i=0; i<array.length; i++)
-    {
-        if (typeof(array[i]) === "number" && !isNaN(array[i]) && array[i] !== null && array[i] !== undefined)
-        newArray.push(array[i]);
+    for (let i = 0; i < array.length; i++) {
+        if (typeof (array[i]) === "number" && !isNaN(array[i]) && array[i] !== null && array[i] !== undefined)
+            newArray.push(array[i]);
     }
     return newArray;
 }
 
-console.log(deleteInvalids([1, null, undefined, 18, -19, NaN, "12", [1, 2], { ob: "lala" }] ));
-console.log(deleteInvalids(["1" , {num:2} , NaN ] ));
-console.log(deleteInvalids([ 1 , 2 , -3 ] ));
-console.log(deleteInvalids({num: [ 1 , 2 , 3 ]}));
-console.log(deleteInvalids(23,54,'ds'));
-console.log(deleteInvalids([true,1,false]));
+console.log(deleteInvalids([1, null, undefined, 18, -19, NaN, "12", [1, 2], { ob: "lala" }]));
+console.log(deleteInvalids(["1", { num: 2 }, NaN]));
+console.log(deleteInvalids([1, 2, -3]));
+console.log(deleteInvalids({ num: [1, 2, 3] }));
+console.log(deleteInvalids(23, 54, 'ds'));
+console.log(deleteInvalids([true, 1, false]));
+
+// task-4
+
+function password(obj) {
+    if (!obj.name || !obj.birthYear || !obj.siteName || typeof obj.birthYear !== 'number' || obj.birthYear < 1000 || obj.birthYear > 9999) {
+        return "invalid";
+    }
+    const name = obj.name;
+    const birthYear = obj.birthYear;
+    const siteName = obj.siteName;
+    const newSiteName = siteName.charAt(0).toUpperCase() + siteName.slice(1);
+    const str = newSiteName + "#" + name + "@" + birthYear;
+    return str;
+}
+
+console.log(password({ name: "kolimuddin", birthYear: 1999, siteName: "google" }))
+console.log(password({ name: "rahat", birthYear: 2002, siteName: "Facebook" } ))
+console.log(password({ name: "toky", birthYear: 200, siteName: "Facebook" }  ))
+console.log(password({ name: "toky", birthYear: 2000}))
